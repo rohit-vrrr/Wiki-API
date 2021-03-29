2f79ab2d982a0f08310518c3eef463771cb67178
Rohit <vr.rohit03@gmail.com>
2021-03-29 13:57:36 +0530

    GET route article

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ const articleSchema = new mongoose.Schema ({
 // creating Model
 const Article = mongoose.model("Article", articleSchema);
 
+
+app.get('/articles', function(req, res) {
+  Article.find(function(err, result) {
+    if(!err) {
+      res.send(result);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("Server listening on port 3000");
 });
